4818b076bfd3552a81434307ad7af1387a418dd6
Vincent Yu <vinjyu1@gmail.com>
2019-04-24 16:10:32 -0400

    using sqlite, user registration and existing emails done

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, session, url_for, redirect
+from flask import Flask, render_template, request, session, url_for, redirect, flash
 from werkzeug import generate_password_hash, check_password_hash
 import csv
 
 app = Flask(__name__)
+app.secret_key = "TTP-FS"
 
 @app.route("/")
 def home():
@@ -11,7 +12,23 @@ def home():
 @app.route("/register/", methods=["GET", "POST"])
 def register():
 	if request.method == "POST":
-		createUser(request.form["name"], request.form["email"], request.form["password"])
+		try:
+			name = request.form["name"]
+			email = request.form["email"]
+			password = request.form["password"]
+			with sqlite3.connect("database.db") as con:
+				cur = con.cursor()
+				#check if email exists
+				email_exists = cur.execute("select email from users where email=?", (email, )).fetchone()
+				if(email_exists):
+					flash("An account with that email already exists.")
+					return redirect(url_for("register"))
+				cur.execute("insert into users (name, email, password) values (?,?,?)", (name, email, password))
+				con.commit()
+		except:
+			con.rollback()
+		finally:
+			con.close()
 		return redirect(url_for("home"))
 	return render_template("register.html")
 
@@ -21,16 +38,3 @@ def login():
 		print(request.form)
 		return render_template("login.html")
 	return render_template("login.html")
-
-def createUser(name, email, password):
-	#Make sure the entered email is unique
-	with open("users.csv", "r", newline="") as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			if row["email"] == email:
-				print("Email has already been registered")
-	#Create user with the given information
-	with open("users.csv", "a", newline="") as csvfile:
-		fieldnames = ["name", "email", "password"]
-		writer = csv.DictWriter(csvfile, fieldnames)
-		writer.writerow({"name": name, "email": email, "password": password})
