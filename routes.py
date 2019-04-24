5d4e3b687d0a9275f711858de93fb78d0dba3bb6
Vincent Yu <vinjyu1@gmail.com>
2019-04-24 16:41:48 -0400

    login verification

--- routes.py
+++ routes.py
@@ -20,7 +20,7 @@ def register():
 				cur = con.cursor()
 				#check if email exists
 				email_exists = cur.execute("select email from users where email=?", (email, )).fetchone()
-				if(email_exists):
+				if email_exists:
 					flash("An account with that email already exists.")
 					return redirect(url_for("register"))
 				cur.execute("insert into users (name, email, password) values (?,?,?)", (name, email, generate_password_hash(password)))
@@ -35,6 +35,22 @@ def register():
 @app.route("/login/", methods=["GET", "POST"])
 def login():
 	if request.method == "POST":
-		print(request.form)
-		return render_template("login.html")
+		try:
+			email = request.form["email"]
+			password = request.form["password"]
+			with sqlite3.connect("database.db") as con:
+				cur = con.cursor()
+				#check if email is associated with an account
+				email_exists = cur.execute("select email from users where email=?", (email,)).fetchone()
+				if email_exists:
+					#verify email and password are correct
+					hashed_password = cur.execute("select password from users where email=?", (email, )).fetchone()[0]
+					if check_password_hash(hashed_password, password):
+						return redirect(url_for("home"))
+				flash("Incorrect username or password.")
+				return redirect(url_for("login"))
+		except:
+			con.rollback()
+		finally:
+			con.close()
 	return render_template("login.html")
