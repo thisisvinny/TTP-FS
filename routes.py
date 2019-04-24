8ff68a56d4ebb1cdb0228bdaaefa2ddfb8d76099
Vincent Yu <vinjyu1@gmail.com>
2019-04-24 05:11:40 -0400

    user registration with name, email, password

--- routes.py
+++ routes.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
+from werkzeug import generate_password_hash, check_password_hash
+import csv
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,10 +10,27 @@ def home():
 
 @app.route("/register/", methods=["GET", "POST"])
 def register():
-	if request.method == "GET":
-		return render_template("register.html")
+	if request.method == "POST":
+		createUser(request.form["name"], request.form["email"], request.form["password"])
+		return redirect(url_for("home"))
+	return render_template("register.html")
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
-	if request.method == "GET":
+	if request.method == "POST":
+		print(request.form)
 		return render_template("login.html")
+	return render_template("login.html")
+
+def createUser(name, email, password):
+	#Make sure the entered email is unique
+	with open("users.csv", "r", newline="") as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			if row["email"] == email:
+				print("Email has already been registered")
+	#Create user with the given information
+	with open("users.csv", "a", newline="") as csvfile:
+		fieldnames = ["name", "email", "password"]
+		writer = csv.DictWriter(csvfile, fieldnames)
+		writer.writerow({"name": name, "email": email, "password": password})
